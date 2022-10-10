152c51aae65ed7e678d03ea8b0cd07b2110ddbe2
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-10-10 19:37:53 +0000

    [Finish] 2-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,10 +31,9 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)  
   end
 
-private
-  # ストロングパラメータ
+ private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)  
   end
   
 end
