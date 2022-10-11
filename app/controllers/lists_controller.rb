8ca801ebe01eab4ea160c5fca1ffced25d85d1c9
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-10-11 10:16:06 +0000

    [Finish] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,12 @@ class ListsController < ApplicationController
 
  # 以下を追加
   def create
-   list = List.new(list_params)
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+       @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
   
   def index
